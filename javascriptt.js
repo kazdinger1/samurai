4eef45ab697d045ab4bd9a1473c8806b95f8a53c
kazdinger1 <91912717+kazdinger1@users.noreply.github.com>
2022-07-04 11:37:04 +0200

    12

---
+++ javascriptt.js
@@ -0,0 +1,48 @@
+function popUpFunkcijaJustice() {
+    document.getElementById("myPopup").classList.toggle("show");
+    document.getElementById("overlay").classList.toggle("show");
+}
+function popUpFunkcijaCourage() {
+    document.getElementById("myPopup3").classList.toggle("show");
+    document.getElementById("overlay3").classList.toggle("show");
+}
+function popUpFunkcijaMercy() {
+    document.getElementById("myPopup7").classList.toggle("show");
+    document.getElementById("overlay7").classList.toggle("show");
+}
+function popUpFunkcijaRespect() {
+    document.getElementById("myPopup8").classList.toggle("show");
+    document.getElementById("overlay8").classList.toggle("show");
+}
+function popUpFunkcijaLoyalty() {
+    document.getElementById("myPopup4").classList.toggle("show");
+    document.getElementById("overlay4").classList.toggle("show");
+}
+function popUpFunkcijaHonor() {
+    document.getElementById("myPopup5").classList.toggle("show");
+    document.getElementById("overlay5").classList.toggle("show");
+}
+function popUpFunkcijaHonesty() {
+    document.getElementById("myPopup6").classList.toggle("show");
+    document.getElementById("overlay6").classList.toggle("show");
+}
+function popUpFunkcijaSelfControl() {
+    document.getElementById("myPopup1").classList.toggle("show");
+    document.getElementById("overlay1").classList.toggle("show");
+}
+
+// jQuery(document).scroll(function () {
+//     var y = jQuery(this).scrollTop();
+//     // if(y>0 && y<10) jQuery('.first_image').animate({right: '100%'});
+//     if (y > 250) {
+//         jQuery('.first_text').animate({right: '110%'});
+//         jQuery('.first_image').animate({bottom: '80%'});
+//     }
+//     if (y > 650) {
+//     jQuery('.image2_text2').animate({right: '94%'});
+//     }
+//     if (y > 1050) {
+//         jQuery('.second_text').animate({right: '100%'});
+//         }
+// });
+
